e1f08e6bde68152c9ac7a56a3f9f66d3562aa72e
lukai <201870058@smail.nju.edu.cn>
2023-06-01 02:15:06 +0800

    打分的后端实现代码

--- src/main/java/com/example/sentistrength/SentistrengthApplication.java
+++ src/main/java/com/example/sentistrength/SentistrengthApplication.java
@@ -3,11 +3,14 @@ package com.example.sentistrength;
 import com.example.sentistrength.service.PathService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import uk.ac.wlv.sentistrength.SentiStrength;
 
 @SpringBootApplication
 public class SentistrengthApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(SentistrengthApplication.class, args);
+        SentiStrength.main("["","","","",""]");
     }
 
 }
