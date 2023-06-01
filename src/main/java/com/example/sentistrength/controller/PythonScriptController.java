f247e210553ab5d950d771245cad9977a1cad9a7
lukai <201870058@smail.nju.edu.cn>
2023-06-01 15:44:48 +0800

    打分的后端实现代码

--- src/main/java/com/example/sentistrength/controller/PythonScriptController.java
+++ src/main/java/com/example/sentistrength/controller/PythonScriptController.java
@@ -50,9 +50,9 @@ public class PythonScriptController {
             File directory = new File("/home/SE3/sentiSpring/data");
             File directory2 = new File("/home/SE3/sentiSpring/result");
             File directory3 = new File("/home/SE3/sentiSpring/proresult");
-            deleteFilesInDirectory(directory);
-            deleteFilesInDirectory(directory2);
-            deleteFilesInDirectory(directory3);
+            deleteFilesAndSubdirectories(directory);
+            deleteFilesAndSubdirectories(directory2);
+            deleteFilesAndSubdirectories(directory3);
             crawlerService.runPythonScript(datesList);
             return new ResultFactory().buildSuccessResult("爬取成功");
         } catch (Exception e) {
@@ -60,18 +60,31 @@ public class PythonScriptController {
         }
     }
 
-    public static void deleteFilesInDirectory(File directory) {
+    public static void deleteFilesAndSubdirectories(File directory) {
         if (directory.isDirectory()) {
-            // directory is a file directory -> get all files
+            // directory is a file directory -> get all files and subdirectories
             File[] entries = directory.listFiles();
             if (entries != null) {
-                // delete all files within this directory
+                // delete all files and subdirectories within this directory
                 for (File entry : entries) {
-                    if (entry.isFile()) {
-                        entry.delete();
-                    }
+                    deleteFileOrDirectory(entry);
                 }
             }
         }
+    }
+
+    public static void deleteFileOrDirectory(File fileOrDirectory) {
+        if (fileOrDirectory.isDirectory()) {
+            // if directory, recursively delete contents
+            for (File child : fileOrDirectory.listFiles()) {
+                deleteFileOrDirectory(child);
+            }
+        }
+        // at this point, fileOrDirectory is either a file, or an empty directory, and can be deleted
+        boolean isDeleted = fileOrDirectory.delete();
+        if (!isDeleted) {
+            System.out.println("Failed to delete: " + fileOrDirectory.getName());
+        }
+    }
     }
 }
