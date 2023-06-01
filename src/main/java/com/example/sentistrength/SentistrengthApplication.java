9e21ebfba754f278906330557a94272922bd1363
lukai <201870058@smail.nju.edu.cn>
2023-06-02 00:00:24 +0800

    生成分析图表并上传

--- src/main/java/com/example/sentistrength/SentistrengthApplication.java
+++ src/main/java/com/example/sentistrength/SentistrengthApplication.java
@@ -13,6 +13,7 @@ public class SentistrengthApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(SentistrengthApplication.class, args);
+
       /*  *//*String[] arg={"trinary","annotateCol","6","overwrite","uploader","E:\\SE3\\dsfw.txt","downloader","E:\\SE3\\dsfw.txt"};
         File directory = new File("E:\\SE3\\dsfw.txt");
         System.out.println(directory.exists());*//*
