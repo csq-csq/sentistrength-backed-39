d12a79b4048f4f4f191f338f1a5aa264be64c84c
lukai <201870058@smail.nju.edu.cn>
2023-06-02 00:04:46 +0800

    生成分析图表并上传

--- src/main/java/com/example/sentistrength/controller/ImageController.java
+++ src/main/java/com/example/sentistrength/controller/ImageController.java
@@ -3,6 +3,7 @@ package com.example.sentistrength.controller;
 import com.example.sentistrength.result.Result;
 import com.example.sentistrength.result.ResultFactory;
 import com.example.sentistrength.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import java.util.List;
 public class ImageController {
 
     private static final String IMAGES_PATH = "/path/to/your/images";
+    @Autowired
     private ImageService imageService;
 
     @GetMapping("/getImage")
